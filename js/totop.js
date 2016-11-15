63819f27cdd4db1b418e7fc0ee76e719f2b50747
xiaotianmei <1025747576@qq.com>
2016-11-15 16:20:17 +0800

    Site updated: 2016-11-15 16:20:13

--- js/totop.js
+++ js/totop.js
@@ -1,29 +1,22 @@
-(function($) {
-    
-    // When to show the scroll link
-    // higher number = scroll link appears further down the page    
-    var upperLimit = 1000; 
-        
-    // Our scroll link element
-    var scrollElem = $('#totop');
-    
-    // Scroll to top speed
-    var scrollSpeed = 500;
-    
-    // Show and hide the scroll to top link based on scroll position    
-    scrollElem.hide();
-    $(window).scroll(function () {             
-        var scrollTop = $(document).scrollTop();        
-        if ( scrollTop > upperLimit ) {
-            $(scrollElem).stop().fadeTo(300, 1); // fade back in            
-        }else{        
-            $(scrollElem).stop().fadeTo(300, 0); // fade out
-        }
-    });
-
-    // Scroll to top animation on click
-    $(scrollElem).click(function(){ 
-        $('html, body').animate({scrollTop:0}, scrollSpeed); return false; 
-    });
-
-})(jQuery);
+window.onload = function() {
+    var obn = document.getElementById('heart');
+    var timer = null;
+    var isTop = true;
+    window.onscroll = function(){
+    	if (!isTop) {
+    		clearInterval(timer);
+    	}
+    	isTop = false;
+    }
+    obn.onclick = function() {
+        timer = setInterval(function() {
+            var osTop = document.documentElement.scrollTop || document.body.scrollTop;
+            var ispeed = Math.floor(-osTop / 5);
+            document.documentElement.scrollTop = document.body.scrollTop = osTop + ispeed;
+            isTop = true;
+            if (osTop == 0) {
+                clearInterval(timer);
+            }
+        }, 30)
+    }
+}
